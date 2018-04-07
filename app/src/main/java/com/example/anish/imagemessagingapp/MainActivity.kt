e12fa23738f1aad9b098109dff25dfdd6b43432a
ani1512 <reachanish1512@gmail.com>
2018-04-08 01:06:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anish/imagemessagingapp/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.anish.imagemessagingapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.example.anish.imagemessagingapp.RegisterActivity
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+
+class MainActivity : AppCompatActivity() {
+
+    internal var mAuth = FirebaseAuth.getInstance()
+    internal var usernameEditText: EditText? = null
+    internal var passwordEditText: EditText? = null
+    internal var incorrectUsernameTextView: TextView? = null
+    internal var incorrectPassword1TextView: TextView? = null
+
+    fun registerClick(view: View){
+
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    fun logIn(view: View){
+
+        if (validate(usernameEditText?.text.toString(), passwordEditText?.text.toString())){
+                mAuth.signInWithEmailAndPassword(usernameEditText?.text.toString(),
+                        passwordEditText?.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+
+                                    val intent = Intent(this, SnapsActivity::class.java)
+                                    startActivity(intent)
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.i("failure", task.exception!!.toString())
+                                    Toast.makeText(this@MainActivity,
+                                            "Authentication failed.", Toast.LENGTH_SHORT).show()
+                                }
+
+                                // ...
+
+                        }
+
+            }
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.password1EditText)
+        incorrectUsernameTextView = findViewById(R.id.incorrectUsernameTextView)
+        incorrectPassword1TextView = findViewById(R.id.incorrectPassword1TextView)
+    }
+
+    fun validate(email:String, password: String): Boolean{
+
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+        if (!email.matches(emailPattern.toRegex()) || email.length == 0) {
+            incorrectUsernameTextView?.text = "Invalid Email!"
+            return false
+        }
+
+        if (password.length < 6) {
+            incorrectUsernameTextView?.text = ""
+            incorrectPassword1TextView?.text = "Password must be 6 characters long!"
+            return false
+        }
+
+        return true
+    }
+}
---
+++ app/src/main/java/com/example/anish/imagemessagingapp/RegisterActivity.kt
@@ -0,0 +1,96 @@
+package com.example.anish.imagemessagingapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    internal var mAuth = FirebaseAuth.getInstance()
+    internal var emailEditText: EditText? = null
+    internal var passwordEditText: EditText? = null
+    internal var cpasswordEditText: EditText? = null
+    internal var incorrectEmailTextView: TextView? = null
+    internal var incorrectPasswordTextView: TextView? = null
+    internal var incorrectCPasswordTextView: TextView? = null
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        cpasswordEditText = findViewById(R.id.confirmEditText)
+        incorrectEmailTextView = findViewById(R.id.incorrectMailTextView)
+        incorrectPasswordTextView = findViewById(R.id.incorrectPasswordTextView)
+        incorrectCPasswordTextView = findViewById(R.id.incorrectCPasswordTextView)
+    }
+
+    private fun signUp() {
+        val intent = Intent(this, SnapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun registerFunction(view: View) {
+
+        if (validate(emailEditText?.text.toString(),
+                passwordEditText?.text.toString(), cpasswordEditText?.text.toString())) {
+            mAuth.createUserWithEmailAndPassword(emailEditText?.text.toString(),
+                    passwordEditText?.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+
+                            signUp()
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i("failure", task.exception!!.toString())
+                            Toast.makeText(this@RegisterActivity,
+                                    "Authentication failed.", Toast.LENGTH_SHORT).show()
+                        }
+
+                        // ...
+                    }
+        }
+
+    }
+
+    fun backToLoginFunction(view: View) {
+        finish()
+    }
+
+    fun validate(email: String, password: String, cpassword: String): Boolean {
+
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+
+        if (!email.matches(emailPattern.toRegex()) || email.length == 0) {
+            incorrectEmailTextView?.text = "Invalid Email!"
+            return false
+        }
+
+        if (password.length < 6) {
+            incorrectEmailTextView?.text = ""
+            incorrectPasswordTextView?.text = "Password must be 6 characters long!"
+            return false
+        }
+
+        if (password != cpassword) {
+            incorrectPasswordTextView?.text = ""
+            incorrectCPasswordTextView?.text = "Incorrect Password!"
+            return false
+        }
+        incorrectCPasswordTextView?.text = ""
+        return true
+    }
+}
