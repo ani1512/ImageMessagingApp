76ffede5caf7a16d2ae3ec2a072541db8b749705
ani1512 <reachanish1512@gmail.com>
2018-04-27 20:29:34 +0530

    App is completed!!

--- app/src/main/java/com/example/anish/imagemessagingapp/ReceiveActivity.kt
+++ app/src/main/java/com/example/anish/imagemessagingapp/ReceiveActivity.kt
@@ -1,55 +1,47 @@
 package com.example.anish.imagemessagingapp
 
-
 import android.app.ProgressDialog
 import android.content.Intent
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.FragmentTransaction
-import android.view.View
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-
+import android.view.View
+import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import kotlinx.android.synthetic.main.nav_header_snaps2.*
 
-import java.util.ArrayList
+class ReceiveActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-class SnapsActivity2 : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    internal var userList: ListView? = null
-    internal var listAdapter: ArrayAdapter<*>? = null
-    internal var emails: ArrayList<String>? = ArrayList()
-    internal var keys: ArrayList<String>? = ArrayList()
-    var progressDialog: ProgressDialog? = null
+    var receiveListView: ListView? = null
+    var receiveArrayList: ArrayList<String> = ArrayList()
+    var adapter: ArrayAdapter<*>? = null
+    var snaps: ArrayList<DataSnapshot>? = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_snaps2)
+        setContentView(R.layout.activity_receive)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
 
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
         val toggle = ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
+
 
         var user: FirebaseUser = FirebaseAuth.getInstance().currentUser!!
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
@@ -57,49 +49,68 @@ class SnapsActivity2 : AppCompatActivity(), NavigationView.OnNavigationItemSelec
 
         var header: View = navigationView.getHeaderView(0)
 
-        var userTextView: TextView = header.findViewById(R.id.userTextView)
+        var userTextView: TextView = header.findViewById<TextView>(R.id.userTextView)
         userTextView.setText(user.email)
 
-        progressDialog = ProgressDialog(this)
-        progressDialog?.setMessage("Loading...")
-        progressDialog?.show()
+        receiveListView = findViewById(R.id.receiveListView)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
+                receiveArrayList)
+        receiveListView!!.adapter = adapter
 
-        userList = findViewById(R.id.userList)
-        listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
-                emails)
-        userList?.adapter = listAdapter
+        FirebaseDatabase.getInstance().getReference().child("users").child(user.uid)
+                .child("snaps").addChildEventListener(object : ChildEventListener{
+            override fun onCancelled(p0: DatabaseError?) {}
+            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {}
+            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {}
+            override fun onChildRemoved(p0: DataSnapshot?) {
 
-        FirebaseDatabase.getInstance().getReference().child("users")
-                .addChildEventListener(object : ChildEventListener {
-                    override fun onCancelled(p0: DatabaseError?) {}
-                    override fun onChildMoved(p0: DataSnapshot?, p1: String?) {}
-                    override fun onChildChanged(p0: DataSnapshot?, p1: String?) {}
-                    override fun onChildRemoved(p0: DataSnapshot?) {}
-                    override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
-                        var email = p0?.child("email")?.value as String
-                        emails!!.add(email)
-                        keys!!.add(p0.key)
-                        Log.i("Key", keys.toString())
-                        listAdapter!!.notifyDataSetChanged()
-                        progressDialog?.dismiss()
+                var index = 0
+                for(snap: DataSnapshot in snaps!!){
+                    if (snap.key == p0?.key){
+                        snaps!!.removeAt(index)
+                        receiveArrayList.removeAt(index)
+                        adapter!!.notifyDataSetChanged()
+                        break
                     }
-                })
+                    index++
+                }
 
-        userList?.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            }
+            override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
+                receiveArrayList.add(p0?.child("from")?.value as String)
+                snaps?.add(p0!!)
+                adapter!!.notifyDataSetChanged()
+            }
 
-            var intent: Intent = Intent(this, SendActivity::class.java)
-            intent.putExtra("key", keys!!.get(position))
-            startActivity(intent)
-        }
+        })
+
+        receiveListView?.onItemClickListener = AdapterView
+                .OnItemClickListener { parent, view, position, id ->
+
+                    var snapshot = snaps!![position]
+
+                    var intent = Intent(this, ViewSnapActivity::class.java)
+
+                    intent.putExtra("imageName",
+                            snapshot.child("imageName").value as String)
+                    intent.putExtra("imageURL", snapshot.child("imageURL").value as String)
+                    intent.putExtra("message", snapshot.child("message").value as String)
+                    intent.putExtra("snapKey", snapshot.key)
+
+                    startActivity(intent)
+                }
 
     }
+
 
     override fun onBackPressed() {
         val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START)
         } else {
-            super.onBackPressed()
+            val intent = Intent(this, SnapsActivity2::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 
@@ -121,16 +132,17 @@ class SnapsActivity2 : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         } else super.onOptionsItemSelected(item)
     }
 
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
+
         val id = item.itemId
 
         if (id == R.id.toHome2) {
-
-        } else if (id == R.id.toSnap2) {
-            val intent = Intent(this, ReceiveActivity::class.java)
+            val intent = Intent(this, SnapsActivity2::class.java)
             startActivity(intent)
             finish()
+        } else if (id == R.id.toSnap2) {
+
         }
         else if(id == R.id.toCamera){
             val intent = Intent(this, CameraActivity::class.java)
